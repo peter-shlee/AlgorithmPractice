664be0b170447ae54056f5248497d63cfed16656
SeungHyun Lee <shlee4290@naver.com>
2021-02-01 01:16:10 +0900

    21/02/01

--- 11053.cpp
+++ 11053.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 using namespace std;
 
+bool can_merge(const vector<int> &a, const vector<int> &b);
+vector<int> merge_vector(const vector<int> &a, const vector<int> &b);
+vector<int> &get_large_vector(vector<int> &a, vector<int> &b);
+
 void print_3_dim_vector(const vector<vector<vector<int> > > &v);
 void print_2_dim_vector(const vector<vector<int> > &v);
 void print_vector(const vector<int> &v);
@@ -16,17 +20,52 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		scanf("%d", &a);
 		nums[i] = a;
+		dp[i][i].push_back(a);
 	}
 
-	int s = 0;
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0, k = i; j < n && k < n; ++j, ++k) {
+	for (int i = 1; i < n; ++i) {
+		for (int j = 0; j + i < n; ++j) {
+			for (int k = j; k < j + i; ++k) {
+				if (can_merge(dp[j][k], dp[k + 1][j + i])) {
+					vector<int> merged_vector = merge_vector(dp[j][k], dp[k + 1][j + i]);
+					dp[j][j + i] = get_large_vector(merged_vector, dp[j][j + i]);
+				} else {
+					vector<int> large_vector = get_large_vector(dp[j][k], dp[k + 1][j + i]);
+					dp[j][j + i] = get_large_vector(large_vector, dp[j][j + i]);
+				}
+			}
 		}
 	}
 
-	print_3_dim_vector(dp);
+	//print_3_dim_vector(dp);
+	printf("%lu\n", dp[0][n - 1].size());
 
 	return 0;
+}
+
+bool can_merge(const vector<int> &a, const vector<int> &b) {
+	return a[a.size() - 1] < b[0];
+}
+
+vector<int> merge_vector(const vector<int> &a, const vector<int> &b) {
+	vector<int> merged_vector(a.size() + b.size());
+	merge(a.begin(), a.end(), b.begin(), b.end(), merged_vector.begin());
+
+	return merged_vector;
+}
+
+vector<int> &get_large_vector(vector<int> &a, vector<int> &b) {
+	if (a.size() > b.size()) {
+		return a;
+	} else if (a.size() < b.size()) {
+		return b;
+	} else {
+		if (a[a.size() - 1] < b[b.size() - 1]) {
+			return a;
+		} else {
+			return b;
+		}
+	}
 }
 
 void print_3_dim_vector(const vector<vector<vector<int> > > &v) {
@@ -35,7 +74,6 @@ void print_3_dim_vector(const vector<vector<vector<int> > > &v) {
 		print_2_dim_vector(*itr);
 	}
 }
-
 
 void print_2_dim_vector(const vector<vector<int> > &v) {
 	for (auto itr = v.begin(); itr != v.end(); ++itr) {
@@ -51,5 +89,3 @@ void print_vector(const vector<int> &v) {
 	}
 	printf("],\t\t\t");
 }
-
-void merge_vector(
