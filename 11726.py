9a79d44f3552199a031583266475279473b80e7e
SeungHyun Lee <shlee4290@naver.com>
2021-06-26 21:35:06 +0900

    21/06/26

--- 11726.py
+++ 11726.py
@@ -6,11 +6,11 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [0] * (n + 2)
+dp = [0] * (n + 1)
 dp[1] = 1
-dp[2] = 3
+dp[2] = 2
 
 for i in range(3, n + 1):
-    dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
 
 print(dp[n])
